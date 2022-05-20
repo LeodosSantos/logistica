bd6578c414b588a0bc648c3c3945372bbd457379
lsantos <leonardo.santos@atendecerto.com.br>
2022-05-20 19:37:18 -0300

    criado banco de dados

--- src/main/java/com/algaworks/algalog/api/controllers/ClienteController.java
+++ src/main/java/com/algaworks/algalog/api/controllers/ClienteController.java
@@ -1,7 +1,9 @@
 package com.algaworks.algalog.api.controllers;
 
-import java.util.Arrays;
 import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,20 +13,17 @@ import com.algaworks.algalog.domain.model.ClienteModel;
 @RestController
 public class ClienteController {
 	
+	@PersistenceContext
+	private EntityManager manager; /*
+									 * é uma interface para fazer as operações com as entidades q serão refletidas
+									 * no banco de dados
+									 */
+	
 	@GetMapping("/clientes")
 	public List<ClienteModel> listar() {
-		var cliente1 = new ClienteModel();
-		cliente1.setId(1L);
-		cliente1.setNome("João da silva");
-		cliente1.setTelefone("21 22323333");
-		cliente1.setEmail("jsilva@teste.com");
+		return manager.createQuery("from ClienteModel", ClienteModel.class)
+				.getResultList();
 		
-		var cliente2 = new ClienteModel();
-		cliente2.setId(1L);
-		cliente2.setNome("Maria da silva");
-		cliente2.setTelefone("21 922225555");
-		cliente2.setEmail("Msilva@teste.com");
-		
-		return Arrays.asList(cliente1, cliente2);
 	}
+		
 }
--- src/main/java/com/algaworks/algalog/domain/model/ClienteModel.java
+++ src/main/java/com/algaworks/algalog/domain/model/ClienteModel.java
@@ -1,10 +1,47 @@
 package com.algaworks.algalog.domain.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity /* representa uma antedate que está associado a uma tabela no banco de dados */
+@Table(name="tb_cliente")
 public class ClienteModel {
-	private long id;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id; /* foi criado para o ID o generade haschcode and equals */
+	
 	private String nome;
+	
 	private String email;
+	
+	@Column(name="fone")	
 	private String telefone;
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (id ^ (id >>> 32));
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ClienteModel other = (ClienteModel) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
 	public long getId() {
 		return id;
 	}
